3a194df6ae567812c79eb8a08e1bc742d5218a65
AlejandroMancilla <137446227+AlejandroMancilla@users.noreply.github.com>
2024-03-19 11:59:47 -0500

    Update Relaciones

--- src/main/java/com/campusland/crudcliente/services/FacturaService.java
+++ src/main/java/com/campusland/crudcliente/services/FacturaService.java
@@ -1,8 +1,16 @@
 package com.campusland.crudcliente.services;
 
+import java.util.List;
+
 import com.campusland.crudcliente.repositories.entities.Factura;
 
 public interface FacturaService {
     
     Factura save(Factura factura);
+
+    Factura findById(Long id);
+
+    List<Factura> findAll();
+
+    Factura delete(Long id);
 }
--- src/main/java/com/campusland/crudcliente/services/ProductoService.java
+++ src/main/java/com/campusland/crudcliente/services/ProductoService.java
@@ -1,8 +1,16 @@
 package com.campusland.crudcliente.services;
 
+import java.util.List;
+
 import com.campusland.crudcliente.repositories.entities.Producto;
 
 public interface ProductoService {
     
     Producto save(Producto producto);
+
+    Producto findById(Long id);
+
+    List<Producto> findAll();
+
+    Producto delete(Long id);
 }
